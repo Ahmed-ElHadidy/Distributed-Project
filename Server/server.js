feec6ccec6ca23f4e8b70d186162b2138fdd07e0
Yousef <yousefshafik234@yahoo.com>
2022-05-13 09:13:38 +0200

    server can track documents now

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,10 @@
 // Imports
 const express = require('express');
+const { all } = require('express/lib/application');
 const http = require('http');
 const {Server} = require('socket.io');
 const {v4:uuidv4} = require('uuid');
+const Document = require('./Document object/Document')
 
 //Setting up Port Number
 const PORTNUM = 3000 || process.env.PORT;
@@ -17,6 +19,8 @@ const server = http.createServer(app);
 const io = new Server(server,{cors:{origin:'*'}});
 
 
+const allDocuments = []
+ 
 //basic routes
 app.get('/',(req,res)=>{
     res.render('./index/index.ejs',{name:'yousef'});
@@ -29,15 +33,55 @@ app.post('/',(req,res)=>{
     let id = uuidv4();
     res.redirect(`/${id}`);
 });
-
+ 
 // handle a connection 
 io.on('connection',(socket)=>{
-    socket.on('id',(id)=>{
-        console.log(id);
+
+    const deleteUser = (docId,userId)=>{
+        const index = allDocuments.findIndex((doc)=>doc.id === docId );
+        if (allDocuments[index].curUsers.length - 1 <= 0){
+            allDocuments[index].curUsers = [];
+            allDocuments.splice(index,1);
+        }
+        else{
+            const userIndex = allDocuments[index].curUsers.findIndex((id)=>id===userId);
+            allDocuments[index].curUsers.splice(userIndex,1);
+            socket.emit('users_changed',allDocuments[index].curUsers);
+        }
+    }
+ 
+    const addUser = (docId,userId)=>{
+        //query my list for document first if it is there connect  to it else 
+        // query database  for document if its there retrive it and add it to list else
+        // create an new entry in database and  new entry in list for the new document
+
+        const index = allDocuments.findIndex((doc)=>doc.id === docId);    
+        if (index === -1){
+        allDocuments.push(new Document(docId,0,[userId],[userId],''))
+        }
+        else{
+            allDocuments[index].curUsers.push(userId);
+            allDocuments[index].allUsers.push(userId);
+        }
+        socket.docId = docId;
+        socket.userId = userId; 
+        console.log(allDocuments);
+        socket.join(docId);
+    }
+
+    
+    socket.on('send_id',(docId,userId)=>{
+        addUser(docId,socket.id); // change it to usersid
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+        deleteUser(socket.docId,socket.userId);
     });
 })
 
-
+ 
 
 //listiening to connections to server
 server.listen(PORTNUM);
+ 
--- public/scripts/document.js
+++ public/scripts/document.js
@@ -1,9 +1,7 @@
 
 
 window.addEventListener('DOMContentLoaded',()=>{
-
+    const documentID = window.location.href.substring(window.location.href.lastIndexOf('/')+1);
     let socket = io();
-    socket.emit('id',window.location.href.substring(window.location.href.lastIndexOf('/')+1))
-    console.log(`${window.location.href.substring(window.location.href.lastIndexOf('/')+1)}`);
-    //io.on('connection')
+    socket.emit('send_id',documentID);
 });
