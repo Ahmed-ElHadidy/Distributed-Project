51c07745148881b84b326814bddaf6b8995093d6
Joseph <Josephdaoud@icloud.com>
2022-06-23 15:27:34 +0200

    Added Home and Editor pages

---
+++ home.js
@@ -0,0 +1,13 @@
+function openDoc(){
+    document.querySelector('.bg-modal').style.display = "flex";
+}
+
+function closePopUp(){
+    document.querySelector('.bg-modal').style.display = "none";
+}
+
+function creteNewDoc(){
+
+window.open('editor.html','_parent');
+
+}
